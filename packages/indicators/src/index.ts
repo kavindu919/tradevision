1889d110876e2c18e7a4708dc96f2a23fb33cf24
kavindu919 <jayakodyk2@gmail.com>
2026-06-18 12:21:01 +0530

    feat: add index and test files for SMA, EMA, RSI, MACD, and Bollinger Bands indicators

---
+++ packages/indicators/src/index.ts
@@ -0,0 +1,7 @@
+export { sma } from "./sma";
+export { ema } from "./ema";
+export { rsi } from "./rsi";
+export { macd } from "./macd";
+export type { MACDResult } from "./macd";
+export { bollingerBands } from "./bollinger";
+export type { BollingerBandsResult } from "./bollinger";
