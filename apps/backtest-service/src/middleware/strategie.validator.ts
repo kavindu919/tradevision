2432a30f5cc060cc1cc1cc1be12c9eaec656bdc8
kavindu919 <jayakodyk2@gmail.com>
2026-06-22 14:47:02 +0530

    fix: convert to prase method use try catch for get validation errors

--- apps/backtest-service/src/middleware/strategie.validator.ts
+++ apps/backtest-service/src/middleware/strategie.validator.ts
@@ -1,26 +1,21 @@
-import { ZodSchema } from "zod";
+import { ZodError, ZodSchema } from "zod";
 import { Request, Response, NextFunction } from "express";
 
 export const validate =
   (schema: ZodSchema<any>) =>
   (req: Request, res: Response, next: NextFunction) => {
-    const result = schema.safeParse({
-      body: req.body,
-      query: req.query,
-      params: req.params,
-    });
-
-    if (!result.success) {
-      const error = result.error.issues[0].message;
-      return res.status(400).json({
-        success: false,
-        message: error,
-      });
+    try {
+      const validatedBody = schema.parse(req.body);
+      req.body = validatedBody;
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const firstErrorMessage = error.issues[0].message;
+        return res.status(400).json({
+          success: false,
+          message: firstErrorMessage,
+        });
+      }
+      next(error);
     }
-
-    req.body = result.data.body ?? req.body;
-    req.query = result.data.query ?? req.query;
-    req.params = result.data.params ?? req.params;
-
-    next();
   };
